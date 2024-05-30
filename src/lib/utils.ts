36b4b6e794d7c5e26f034510d5103d69ae982b17
Oauzz <ouzama.pro@outlook.com>
2024-05-30 10:58:11 +0100

    thumbnail-favicon

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { Metadata } from 'next'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -11,4 +12,47 @@ export function absoluteUrl(path: string) {
   return `http://localhost:${
     process.env.PORT ?? 3000
   }${path}`
+}
+
+export function constructMetadata({
+  title = "ValueXp.",
+  description = "Suivez et estimez facilement les prix immobiliers.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+  noIndex = false
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@Ouazz"
+    },
+    icons,
+    metadataBase: new URL('https://valuexp-oauzs-projects.vercel.app/'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
 }
